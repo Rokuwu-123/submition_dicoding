4579294f77181d887e5c8f4f1ef00b9d175abcb7
rokuwu <bimapradipta99@gmail.com>
2022-06-05 18:01:09 +0700

    update exports. ke module.exports

--- app/libraries/libraries.js
+++ app/libraries/libraries.js
@@ -1,8 +1,12 @@
-exports.hitung_object = async(vObject)=>{
-    jm_data = 0;
-    for (const vKey in vObject) {
-       jm_data++;
-    };
+libraries = {
+    hitung_object : async(vObject)=>{
+        jm_data = 0;
+        for (const vKey in vObject) {
+           jm_data++;
+        };
+    
+        return jm_data;
+    }
+};
 
-    return jm_data;
-}
+module.exports = libraries;
--- app/models/models.js
+++ app/models/models.js
@@ -1,42 +1,46 @@
 const { nanoid } = require('nanoid');
 
-exports.simpan_baru = async(data_request)=>{
+models = {
+    simpan_baru : async(data_request)=>{
 
-    return {
-        "id": nanoid(),
-        "name": data_request.name,
-        "year": data_request.year,
-        "author": data_request.author,
-        "summary": data_request.summary,
-        "publisher": data_request.publisher,
-        "pageCount": data_request.pageCount,
-        "readPage": data_request.readPage,
-        "finished": data_request.readPage == data_request.pageCount ? true : false,
-        "reading": false,
-        "insertedAt": new Date().toISOString(),
-        "updatedAt": new Date().toISOString()
-    };
+        return {
+            "id": nanoid(),
+            "name": data_request.name,
+            "year": data_request.year,
+            "author": data_request.author,
+            "summary": data_request.summary,
+            "publisher": data_request.publisher,
+            "pageCount": data_request.pageCount,
+            "readPage": data_request.readPage,
+            "finished": data_request.readPage == data_request.pageCount ? true : false,
+            "reading": false,
+            "insertedAt": new Date().toISOString(),
+            "updatedAt": new Date().toISOString()
+        };
+    },
+
+    data_buku : async(daftar_buku)=>{
+
+        let data_kembali = []
+    
+        daftar_buku.forEach(data => {
+            vdata_buku = {
+                id : data.id,
+                name : data.name,
+                publisher : data.publisher
+            }
+    
+            data_kembali.push(vdata_buku)
+        });
+    
+        return data_kembali
+    },
+    
+    detail_buku :async(daftar_buku,parameter)=>{
+        let detail_data = daftar_buku.find(data=> data.id == parameter.id_buku)
+    
+        return detail_data
+    }
 };
 
-exports.data_buku = async(daftar_buku)=>{
-
-    let data_kembali = []
-
-    daftar_buku.forEach(data => {
-        vdata_buku = {
-            id : data.id,
-            name : data.name,
-            publisher : data.publisher
-        }
-
-        data_kembali.push(vdata_buku)
-    });
-
-    return data_kembali
-};
-
-exports.detail_buku = async(daftar_buku,parameter)=>{
-    let detail_data = daftar_buku.find(data=> data.id == parameter.id_buku)
-
-    return detail_data
-}
+module.exports = models;
